79dd576b059c1171234b4a9b2b2315cf3f8cddea
Benyamain <yacoobbenyamain@gmail.com>
2023-05-18 02:54:00 -0400

    Setting up the database

---
+++ app/src/main/java/com/example/notes/models/Score.kt
@@ -0,0 +1,13 @@
+package com.example.notes.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "scores_table")
+data class Score(
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "score") val score: String?,
+    @ColumnInfo(name = "date") val date: String?
+)
